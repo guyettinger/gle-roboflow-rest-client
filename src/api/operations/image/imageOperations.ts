9228a9560aa2521860348479685117a3aa54de96
guyettinger <guy@pointivo.com>
2023-11-14 19:25:27 -0500

    v1.0.4

--- src/api/operations/image/imageOperations.ts
+++ src/api/operations/image/imageOperations.ts
@@ -37,7 +37,7 @@ export class ImageOperations extends OperationsModel {
                 formData,
                 {
                     headers: {
-                        "Content-Type": "application/x-www-form-urlencoded"
+                        "Content-Type": "multipart/form-data"
                     }
                 }
             )
